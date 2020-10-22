bb11fcd5a020e3443b22a3baf5f912a4b2b5076a
ButArtur <73294457+ButArtur@users.noreply.github.com>
2020-10-23 01:02:26 +0700

    Add files via upload

--- solution.js
+++ solution.js
@@ -1,36 +1,92 @@
-// Create matrix
-			var x = 100;
-			var y = 100;
+
+				var array=null;				
+				var inputMatrixSize=null;
+				var N = null;				
+				var inputCoorX = null;
+				var inputCoorY = null;
+				var coordinateX = null;
+				var coordinateY = null;
+				var str='';
+				var tbl='';
+			
+			////////////////Create matrix////////////////////////////
 			function Matrix(columns,rows) {
 				var x = new Array(columns);
-					for (var j = 1; j < columns; j++) {
+					for (var j = 0; j < columns; j++) {
 						x[j] = new Array(rows);
 					}
 				return x;
 			}
+			////////////////Output Table////////////////////////////
+			function MatrixBuild(){
+				 str='';
+				 tbl='';
+				 inputMatrixSize = document.getElementById("matrixSize");				 
+				 N = Number(inputMatrixSize.value)+1;
+				 
+				array = Matrix(N,N);		
+				
+				array[1][1]=1;
+				//Put to the matrix first row
+					for (var j = 2; j < N; j++) {
+						array[j][1]=array[j-1][1]+(j-1);
+					}
+				//Put to the matrix all rows
+					for(var i=2; i<N; i++){  
+						for(var j=1; j<N; j++){
+							array[j][i] = array[j][i-1]+(i+j-1) ;  
+						}	
+					}	
+					for(i=N-1;i>0;i--){
+						str=' ';
+						for(j=1;j<N;j++){
+							str+='<td>'+array[j][i]+'</td>';		
+						}
+							tbl+='<tr>'+str+'</tr>'
+					
+					
+					
+					}
+				document.getElementById("myTable").innerHTML=tbl;
+			}
 			
-			var array = Matrix(x,y);		
-			var str='';
-			array[1][1]=1;
-			//Put to the matrix first row
-			for (var j = 2; j < x; j++) {
-				array[j][1]=array[j-1][1]+(j-1);
+			
+			
+			
+		/////////////////////////Search ID/////////////////////////////////////	
+			function Search(){
+								
+				inputCoorX = document.getElementById("coordinateX");
+				inputCoorY = document.getElementById("coordinateY");
+				coordinateX = inputCoorX.value;
+				coordinateY = inputCoorY.value;
+				MatrixBuild();
+				tbl='';
+				str='';
+				for(i=N-1;i>0;i--){
+						str=' ';
+						for(j=1;j<N;j++){
+							if(j==coordinateX&&i==coordinateY){
+								str+='<td bgcolor="#f75216">'+array[j][i]+'</td>';
+							}
+							else if(j<=coordinateX&&i<=coordinateY){
+								str+='<td bgcolor="#e0af34">'+array[j][i]+'</td>';	
+							}
+							else
+								str+='<td>'+array[j][i]+'</td>';
+														
+						}
+							tbl+='<tr>'+str+'</tr>'
+					
+					
+					document.getElementById("myTable").innerHTML=tbl;
+					}
+				
+				document.getElementById("treasuresID").innerHTML='Treasures id: '+array[coordinateX][coordinateY];
+				
+				
 			}
-			//Put to the matrix all rows
-			for(var i=2; i<y; i++){  
-				for(var j=1; j<y; j++){
-					array[j][i] = array[j][i-1]+(i+j-1) ;
-	  
-	  
-				}	
-			}
-			// Output matrix to screen
-			for(i=1;i<x;i++){
-				str=' ';
-				for(j=1;j<y;j++){
-					str+=array[j][i]+' ';		
-				}
-					document.write(str+'<br \/>');
-			}
-  
-			document.write('<br \/>'+array[10][5]);
+			
+			
+			
+			
